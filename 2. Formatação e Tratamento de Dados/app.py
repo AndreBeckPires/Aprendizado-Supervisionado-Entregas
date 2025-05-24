139cc213d4a838e8ade643145dad79fd712c111a
unknown <andrebeckpires@gmail.com>
2025-05-24 16:49:03 -0300

    exercicio 2

--- 2. Formatação e Tratamento de Dados/app.py
+++ 2. Formatação e Tratamento de Dados/app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from collections import Counter
 
+
 def read_csv(path):
     df = pd.read_csv(path)
 
@@ -36,16 +37,28 @@ def number_pattern(df,column):
 
 def date(df,column):
     df[column] = pd.to_datetime(df[column], errors = 'coerce')
+    df[column].fillna(df[column].median(), inplace = True)
     return df
 
-df = read_csv('1. Formatação e Tratamento de Dados\dataset_clientes.csv')
+def fix_sim_nao(df,column):
+    df[column] = df[column].apply(lambda x : 'Sim' if x and  x == 'S' else x)
+    df[column] = df[column].apply(lambda x : 'Não' if x and  x == 'N' else x)
+    return df
+
+df = read_csv('2. Formatação e Tratamento de Dados\exercicio2.csv')
 df = string_pattern(df,'nome')
-df = string_pattern(df,'cidade')
 df = string_pattern(df,'ativo')
 df = email(df)
 df = number_pattern(df,'idade')
-df = number_pattern(df,'compras')
-df = date(df,'data_cadastro')
-df = df.dropna()
-save_to_csv(df,'1. Formatação e Tratamento de Dados\corrigido.csv')
-#print(df.head())
+df = number_pattern(df,'salario')
+df = number_pattern(df,'projetos_entregues')
+df = date(df,'data_admissao')
+df = fix_sim_nao(df,'ativo')
+
+save_to_csv(df,'2. Formatação e Tratamento de Dados\corrigido.csv')
+
+
+
+
+
+# Inconsistências no status "ativo": "Sim", "Não", "s", "n".
